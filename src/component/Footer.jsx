f3b6122d444b01e8d3f05821921e4f517a9381f4
shifachaus <chausshifa804@gmail.com>
2025-12-28 12:47:17 +0530

    style: update footer design

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,20 +1,19 @@
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="  bg-gray-100  ">
+    <footer className=" ">
       <div className="mx-auto max-w-7xl  grid grid-cols-1 space-y-10 bg-primary px-5 py-8 pt-20  tracking-wider text-black md:grid-cols-4 md:space-y-0 xl:px-28 xl:py-20 ">
-        <section className="space-y-4">
-          <p className="w-2/3 text-sm font-light">
+        <section className="space-y-4 md:mt-12">
+          <p className="w-2/3 text-sm font-light text-gray-400">
             400 University Drive Suite 200 Coral Gables, FL 33134 USA
           </p>
         </section>
 
         <section className="space-y-6">
           <div className="space-y-1">
-            <h2 className="uppercase ">Userful links</h2>
-            <div className="w-1/6 border-t"></div>
+            <h2 className="capitalize text-gray-400 font-light"> links</h2>
           </div>
-          <div className="flex flex-col space-y-4 text-sm font-light">
+          <div className="flex flex-col space-y-4 text-sm font-medium">
             <a href="#">Home</a>
             <a href="#">Shop</a>
             <a href="#">About</a>
@@ -24,27 +23,25 @@ const Footer = () => {
 
         <section className="space-y-6">
           <div className="space-y-1">
-            <h2 className="uppercase ">Help</h2>
-            <div className="w-1/6 border-t"></div>
+            <h2 className="capitalize text-gray-400 font-light">Help</h2>
           </div>
-          <div className="flex flex-col space-y-4 text-sm font-light">
+          <div className="flex flex-col space-y-4 text-sm font-medium">
             <a href="#">Privacy Policies</a>
           </div>
         </section>
 
         <section className="space-y-6">
           <div className="space-y-1">
-            <h2 className="uppercase ">Newsletter</h2>
-            <div className="w-1/6 border-t"></div>
+            <h2 className="capitalize text-gray-400 font-light">Newsletter</h2>
           </div>
           <div className="flex flex-col space-y-4 text-sm font-light">
             <div className="flex gap-2 items-center">
               <input
                 type="email"
                 placeholder="Enter Your Email Address"
-                className="appearance-none bg-gray-100 border-b border-black w-full py-1 px-1 leading-tight focus:outline-none focus:shadow-outline text-gray-700 "
+                className="appearance-none border-b border-black w-full py-1 px-1 leading-tight focus:outline-none focus:shadow-outline text-gray-700 "
               />
-              <button className="bg-gray-100 border-b border-black font-medium leading-tight py-1 px-1">
+              <button className="border-b border-black font-medium leading-tight py-1 px-1">
                 SUBSCRIBE
               </button>
             </div>
@@ -53,7 +50,7 @@ const Footer = () => {
       </div>
 
       <div className="mx-auto max-w-7xl ">
-        <p className=" border-t  p-4 text-black">
+        <p className=" border-t  p-4 text-sm text-gray-800">
           {year} ShopEase. All rights reverved
         </p>
       </div>
